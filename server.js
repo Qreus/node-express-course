42c8fc6a8aad03fb29cd84bedc01b06ef76c41ae
Qreus <77340109+Qreus@users.noreply.github.com>
2021-01-19 23:47:44 +0800

    add first GET route

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const middleware = require('./middleware.js');
+
+app.use(bodyParser.json())
 
 const mockUserData=[
-{name:'Mark'},
-{name:'Jill'}
+	{name:'Mark'},
+	{name:'Jill'}
 ]
-
-app.get('/users', function(req,res){
-res.json({
-success: true,
-message: 'successfully got users. Nice!',
-users: mockUserData
+app.get('/users',function(req,res){
+	res.json({
+		success: true,
+		message: 'successfully got users. Nice!',
+		users: mockUserData
+	})
 })
-})
-
 app.listen(8000,function(){console.log('server is listening')})
